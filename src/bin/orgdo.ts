33a53ab3a89d00a29c6605a67ac04fb84c103179
Sigoden Huang <sigoden@gmail.com>
2018-10-15 09:58:13 +0800

    add update notifier

--- src/bin/orgdo.ts
+++ src/bin/orgdo.ts
@@ -2,6 +2,9 @@
 
 import * as yargs from "yargs";
 import * as path from "path";
+import * as updateNotifier from "update-notifier";
+
+updateNotifier({ pkg: require("../../package.json") }).notify();
 
 yargs
   .commandDir("cmds", {
