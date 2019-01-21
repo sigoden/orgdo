aa6dc007257b7d5f67dba9f82e0669cad5c905d5
sigoden <sigoden@gmail.com>
2019-01-21 10:27:53 +0800

    fix: config path .orgdo/config.json

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import * as os from "os";
 import * as path from "path";
 
-export const DATA_DIR = path.resolve(os.homedir(), "orgdo");
+export const DATA_DIR = path.resolve(os.homedir(), ".orgdo");
 export const DATA_FILE = path.resolve(DATA_DIR, "orgdo.json");
 export const PID_FILE = path.resolve(DATA_DIR, "orgdo.pid");
 export const IPC_APP_SPACE = "orgdo.";
